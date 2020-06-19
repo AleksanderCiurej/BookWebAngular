5af7bf781da9557dbc5d83ab0b7452614fb453c2
Aleksander <aleksander.ciurej@gmail.com>
2020-06-19 17:04:06 +0200

    books

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,7 +1,5 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
-import {map} from "rxjs/operators";
-import {Token} from "../models/token";
+import {HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
